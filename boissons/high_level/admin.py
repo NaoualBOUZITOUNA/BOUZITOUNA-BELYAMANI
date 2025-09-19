4deaaded7b482b848a55c3e9013595c35def24da
NaoualBOUZITOUNA <naoualbouzitouna2000@gmail.com>
2025-09-19 17:22:52 +0200

    Update admin.py

--- boissons/high_level/admin.py
+++ boissons/high_level/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+from . import models
+admin.site.register(models.QuantiteMatierePremiere)
+admin.site.register(models.localisation)
+admin.site.register(models.local
+admin.site.register(models.Energie)
+admin.site.register(models.DebitEnergie)
+admin.site.register(models.Produit)
+admin.site.register(models.MatierePremiere)
+admin.site.register(models.QuantiteMatierePremiere)
+admin.site.register(models.UtilisationMatierePremiere)
+admin.site.register(models.Metier)
+admin.site.register(models.RessourceHumaine)
+admin.site.register(models.machine)
