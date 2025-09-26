e6b582ff746a41069c9817338aa7326f69e3a521
NaoualBOUZITOUNA <naoualbouzitouna2000@gmail.com>
2025-09-26 08:26:50 +0200

    Update tests.py

--- boissons/high_level/tests.py
+++ boissons/high_level/tests.py
@@ -1,1 +1,9 @@
 # Create your tests here.
+from django.test import TestCase
+
+
+class CostCalculationTestCase(TestCase):
+    def test_local_costs(self):
+        self.assertEqual(Metier.objects.count(), 0)
+        Metier.objects.create(nom="Mélangeur", remuneration=10)
+        self.assertEqual(Metier.objects.count(), 1)
